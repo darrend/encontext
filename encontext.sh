14c8636999d9d3d7bf1bbe16a1410c6e54474f7a
Darren Day <darrend@missiondata.com>
2016-11-12 17:56:00 -0500

    do it all in 1 function

--- encontext.sh
+++ encontext.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-__encontext_sanitize() {
-  __context=${1//[^a-zA-Z0-9]/} # http://stackoverflow.com/questions/89609/in-a-bash-script-how-do-i-sanitize-user-input
-}
-
-__encontext_create() {
-  mkdir -p ~/.encontext
-  __encontext_sanitize $1
-  ln -s $(pwd -P) ~/.encontext/$__context
-  (>&2 echo "creating new context ${1}") # http://stackoverflow.com/questions/2990414/echo-that-outputs-to-stderr
-}
-
 encontext () {
-  __encontext_sanitize $1
-  (cd -P ~/.encontext/$__context) || __encontext_create $__context
-  export ENCNX_NAME=$__context
+  context=${1//[^a-zA-Z0-9]/} # http://stackoverflow.com/questions/89609/in-a-bash-script-how-do-i-sanitize-user-input
+  context_path=~/.encontext/$context
+
+  if [ -d $context_path ]
+  then
+    cd -P $context_path
+  else
+    mkdir -p ~/.encontext
+    ln -s $(pwd -P) $context_path
+    (>&2 echo "creating new context ${context}") # http://stackoverflow.com/questions/2990414/echo-that-outputs-to-stderr
+  fi
+  export ENCNX_NAME=$context
   export ENCNX_PATH=$(pwd -P)
 }
 
@@ -25,3 +23,4 @@ alias e='mvim --servername $ENCNX_NAME --remote-silent'
 alias c='cd $ENCNX_PATH'
 alias w='echo $ENCNX_PATH'
 alias n='echo $ENCNX_NAME'
+alias x='echo $ENCNX_NAME $ENCNX_PATH'

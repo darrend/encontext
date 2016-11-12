6c33e758dc909cb81e51f07cab0ca95f502e1f8e
Darren Day <darrend@missiondata.com>
2016-11-12 17:41:45 -0500

    do it with symlinks

--- encontext.sh
+++ encontext.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-encontext () {
-  export ENCNX_NAME=$1
-  export ENCNX_PATH=$(pwd)
+__encontext_sanitize() {
+  __context=${1//[^a-zA-Z0-9]/} # http://stackoverflow.com/questions/89609/in-a-bash-script-how-do-i-sanitize-user-input
 }
+
+__encontext_create() {
+  mkdir -p ~/.encontext
+  __encontext_sanitize $1
+  ln -s $(pwd -P) ~/.encontext/$__context
+  (>&2 echo "creating new context ${1}") # http://stackoverflow.com/questions/2990414/echo-that-outputs-to-stderr
+}
+
+encontext () {
+  __encontext_sanitize $1
+  (cd -P ~/.encontext/$__context) || __encontext_create $__context
+  export ENCNX_NAME=$__context
+  export ENCNX_PATH=$(pwd -P)
+}
+
 
 # edit in context
 alias e='mvim --servername $ENCNX_NAME --remote-silent'
